17ab010e73816347ce6f71cbba61f2f75be23ff4
KarolKiraga <karolkiraga@gmail.com>
2020-04-09 22:15:18 +0200

    First Commit,

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,44 @@
 package com.kodilla.testing;
 
+        import com.kodilla.testing.calculator.Calculator;
+        import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+        System.out.println("Test Calculator");
+
+        Calculator calculator = new Calculator();
+        Calculator calculator2 = new Calculator();
+
+
+        int sumTest = calculator.add(3,2) - 2;
+        int subtractTest = calculator.subtract(8,4) + 4;
+
+        if (sumTest == 3){
+
+            System.out.println("Correct add operation");
+        }
+        else{
+
+            System.out.println("Incorrect add operation");
+        }
+
+        if (subtractTest == 8){
+
+            System.out.println("Correct subtract operation");
+        }
+        else{
+
+            System.out.println("Incorrect subtract operation");
+        }
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,23 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+    int a,b;
+    public Calculator (){
+
+        this.a = a;
+        this.b = b;
+
+    }
+
+    public int add(int a, int b){
+
+        int sum = a + b;
+        return sum;
+    }
+    public int subtract(int a, int b){
+
+        int subtract = a - b;
+        return subtract;
+    }
+
+}
